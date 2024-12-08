e201db024282419cb20802c8e301a94b09ff9ab2
Shubham Thakre <95696721+thakre2001@users.noreply.github.com>
2024-12-09 02:03:18 +0530

    Add files via upload

---
+++ src/Services/ContactServices.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export class ContactServices{
+ static serverURL=`http://localhost:8000`
+  static getAllContacts(){
+    let dataURL=`${this.serverURL}/contacts`
+    return axios.get(dataURL)
+  }
+
+  static getContact(contactId){
+    let dataURL=`${this.serverURL}/contacts/${contactId}`
+    return axios.get(dataURL)
+  }
+
+  static createContact(contact){
+    let dataURL=`${this.serverURL}/contacts`
+    return axios.post(dataURL,contact)
+  }
+
+  static updateContact(contact,contactId){
+    let dataURL=`${this.serverURL}/contacts/${contactId}`
+    return axios.put(dataURL,contact)
+  }
+
+  static deleteContact(contactId){
+    let dataURL=`${this.serverURL}/contacts/${contactId}`
+    return axios.delete(dataURL)
+  }
+}
