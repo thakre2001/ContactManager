8572644d4d52b75f45269ab42781541af4e57ecc
Shubham Thakre <95696721+thakre2001@users.noreply.github.com>
2024-12-09 02:00:25 +0530

    Add files via upload

---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='bg-dark p-3'>
+      <Link to={'/Contacts/list'} className='navbar-brand text-light ms-5'><i className='fa fa-mobile text-warning ms-5+ me-2'/>Contact <span className='text-warning'>Manager</span></Link>
+    </div>
+  )
+}
+
+export default Navbar
