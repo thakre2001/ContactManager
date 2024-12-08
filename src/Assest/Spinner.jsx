066cccab5c3b6e161226172d34418a4058e239de
Shubham Thakre <95696721+thakre2001@users.noreply.github.com>
2024-12-09 01:59:06 +0530

    Add files via upload

---
+++ src/Assest/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from './amalie-steiness.gif'
+
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spinner} alt="" className='' />
+    </div>
+  )
+}
+
+export default Spinner
